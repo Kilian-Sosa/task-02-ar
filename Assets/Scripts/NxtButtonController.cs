213e5fb3813c3518779e5c1c293fa1ff51b0ce69
Kilian Sosa Guillén <85161810+Kilian-Sosa@users.noreply.github.com>
2024-01-12 17:58:54 +0000

    fix

--- Assets/Scripts/NxtButtonController.cs
+++ Assets/Scripts/NxtButtonController.cs
@@ -1,6 +1,5 @@
 using TMPro;
 using UnityEngine;
-using UnityEngine.XR.ARFoundation;
 
 public class NxtButtonController : MonoBehaviour {
     private GameObject canvasEditor;
@@ -20,7 +19,7 @@ public class NxtButtonController : MonoBehaviour {
             canvasEditor.SetActive(false);
             GameObject canvasScore = GameObject.Find("CanvasScore");
             canvasScore.SetActive(true);
-            text.text = $"Llaves: 0/{GameManager.instance.gameObjects.Count}";
+            text.text = $"Llaves: 0/{GameManager.instance.gameObjects.Count - 1}";
             PlayerPrefs.SetInt("mode", 0);
         }
     }
