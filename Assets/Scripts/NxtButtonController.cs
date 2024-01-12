73fbc50b86d9d0bef6cd27025b7bf94609b47e9e
Kilian Sosa Guillén <85161810+Kilian-Sosa@users.noreply.github.com>
2024-01-12 19:05:22 +0000

    fix

--- Assets/Scripts/NxtButtonController.cs
+++ Assets/Scripts/NxtButtonController.cs
@@ -14,6 +14,7 @@ public class NxtButtonController : MonoBehaviour {
     public void Next() {
         if (GameManager.instance.gameObjects.Count < 2) return;
         if (++GameManager.instance.confStage == 1) {
+            GameManager.instance.maxKeys = GameManager.instance.gameObjects.Count;
             canvasEditor.transform.Find("Item/SM_Key UI").gameObject.SetActive(false);
             canvasEditor.transform.Find("Item/Lock UI").gameObject.SetActive(true);
         }
@@ -21,7 +22,6 @@ public class NxtButtonController : MonoBehaviour {
             mode.text = GameManager.instance.confStage.ToString();
             canvasEditor.SetActive(false);
             canvasScore.SetActive(true);
-            GameManager.instance.maxKeys = GameManager.instance.gameObjects.Count - 1;
             text.text = $"Llaves: 0/{GameManager.instance.maxKeys}";
             PlayerPrefs.SetInt("mode", 0);
             GameObject.Find("XR Origin").GetComponent<ARPlaneManager>().SetTrackablesActive(false);
--- Assets/Scripts/OnMouseKey.cs
+++ Assets/Scripts/OnMouseKey.cs
@@ -4,8 +4,8 @@ public class OnMouseKey : MonoBehaviour
 {
     private void OnMouseEnter() {
         if (PlayerPrefs.GetInt("mode") == 1) return;
-        GameManager.instance.gameObjects.Remove(gameObject);
+        GameManager.instance.gameObjects.Remove(this.gameObject);
         GameManager.instance.AddScore();
-        Destroy(gameObject);
+        Destroy(this.gameObject);
     }
 }
