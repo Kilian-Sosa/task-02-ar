7e33c8c4b14f1405cb3a4e2304bcc495a602e24b
Soniko <sonikonyas@gmail.com>
2024-01-12 18:25:11 +0000

    ButtonFix

--- Assets/Scripts/NxtButtonController.cs
+++ Assets/Scripts/NxtButtonController.cs
@@ -5,6 +5,7 @@ using UnityEngine.XR.ARFoundation;
 public class NxtButtonController : MonoBehaviour {
     private GameObject canvasEditor;
     [SerializeField] TextMeshProUGUI text, mode;
+    [SerializeField] GameObject canvasScore;
 
     private void Start() {
         canvasEditor = GameObject.Find("CanvasEditor");
@@ -19,7 +20,6 @@ public class NxtButtonController : MonoBehaviour {
         if (GameManager.instance.lockSet) {
             mode.text = GameManager.instance.confStage.ToString();
             canvasEditor.SetActive(false);
-            GameObject canvasScore = GameObject.Find("CanvasScore");
             canvasScore.SetActive(true);
             text.text = $"Llaves: 0/{GameManager.instance.gameObjects.Count - 1}";
             PlayerPrefs.SetInt("mode", 0);
